cd64bcef59aa2ab776f2169a8e51e4d6ef7a717d
johnnonymous <94593509+bayodmaryjane@users.noreply.github.com>
2022-01-01 11:30:34 +0800

    01012022

--- Final Project/app/src/main/java/com/example/knowtech/Signin.java
+++ Final Project/app/src/main/java/com/example/knowtech/Signin.java
@@ -2,24 +2,47 @@ package com.example.knowtech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class Signin extends AppCompatActivity {
 
+    TextView textView;
+    Button signin_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-        Button signin;
-        EditText signin_username, signin_password;
+        getSupportActionBar();
 
-        signin_username = (EditText) findViewById(R.id.signin_username);
-        signin_password = (EditText) findViewById(R.id.signin_password);
-        signin = (Button) findViewById(R.id.signin);
+        TextView textview = findViewById(R.id.reg_signin);
+        textview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Signin.this,Signup.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+
+        signin_button = findViewById(R.id.btn_signin);
+        signin_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent int1 = new Intent(Signin.this,Homepage.class);
+                startActivity(int1);
+
+                finish();
+            }
+        });
 
 
 
--- Final Project/app/src/main/java/com/example/knowtech/Signup.java
+++ Final Project/app/src/main/java/com/example/knowtech/Signup.java
@@ -2,15 +2,44 @@ package com.example.knowtech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
+
+    TextView textView;
+    Button signup_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        getSupportActionBar();
 
+        TextView textView = findViewById(R.id.reg_signup);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Signup.this,Signin.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+
+        signup_register = findViewById(R.id.signup_register);
+        signup_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Signup.this,Homepage.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
     }
 }
