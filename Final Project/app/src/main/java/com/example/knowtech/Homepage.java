3c945ea88b1d857b5d2a0a14838e4caffba1524c
sarahrls <94595287+sarahrls@users.noreply.github.com>
2022-01-02 02:05:18 +0800

    updated

--- Final Project/app/src/main/java/com/example/knowtech/Homepage.java
+++ Final Project/app/src/main/java/com/example/knowtech/Homepage.java
@@ -1,8 +1,13 @@
 package com.example.knowtech;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Homepage extends AppCompatActivity {
 
@@ -10,6 +15,10 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView2);
+        NavController navController = Navigation.findNavController(this, R.id.fragmentContainerView4);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
 
     }
