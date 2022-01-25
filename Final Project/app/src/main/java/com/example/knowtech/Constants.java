8be4da13d5b56e8ab97bdc02505804045c199bc2
johnnonymous <94593509+bayodmaryjane@users.noreply.github.com>
2022-01-25 20:04:29 +0800

    updated

--- Final Project/app/src/main/java/com/example/knowtech/Constants.java
+++ Final Project/app/src/main/java/com/example/knowtech/Constants.java
@@ -3,7 +3,7 @@ package com.example.knowtech;
 
 public class Constants {
 
-    public static final String BASE_URL = "https://knowtech-study-room.000webhostapp.com/";
+    public static final String BASE_URL = "https://knowtech-study.000webhostapp.com/";
     public static final String PREFERENCE_NAME = "session";
     public static final String KEY_ISE_LOGGED_IN = "isLoggedIn";
     public static final String KEY_EMAIL = "email";
