109c1d8842937354afe334c50bf338fce1e5e29e
sarahrls <94595287+sarahrls@users.noreply.github.com>
2022-01-29 21:11:20 +0800

    updated

---
+++ Final Project/app/src/main/java/com/example/knowtech/RoomCC103.java
@@ -0,0 +1,14 @@
+package com.example.knowtech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoomCC103 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_cc103);
+    }
+}
--- Final Project/app/src/main/java/com/example/knowtech/Rooms.java
+++ Final Project/app/src/main/java/com/example/knowtech/Rooms.java
@@ -4,11 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,29 +21,79 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.knowtech.databinding.ActivityRoomsBinding;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Rooms extends AppCompatActivity {
-    private CardView RoomCC101;
+    selectYear YearAction;
+    selectFirstYear FirstYearAction;
+    selectSecondYear SecondYearAction;
+    selectThirdYear ThirdYearAction;
+    selectFourthYear FourthYearAction;
+    Spinner spinner;
+    List<String> names;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rooms);
 
-        RoomCC101 = findViewById(R.id.RoomCC101_Card);
-        RoomCC101.setOnClickListener(new View.OnClickListener() {
+        spinner=findViewById(R.id.Spinner);
+        YearAction=new selectYear();
+        FirstYearAction=new selectFirstYear();
+        SecondYearAction=new selectSecondYear();
+        ThirdYearAction=new selectThirdYear();
+        FourthYearAction=new selectFourthYear();
 
+        names=new ArrayList<>();
+        names.add("Select Year");
+        names.add("First Year");
+        names.add("Second Year");
+        names.add("Third Year");
+        names.add("Fourth Year");
+
+
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(Rooms.this, R.layout.item, names);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Intent i;
-
-                switch(v.getId()){
-                    case R.id.RoomCC101_Card:i=new Intent (v.getContext(), RoomCC101.class);startActivity(i);
-                    default:break;
-
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        selectFragment(YearAction);
+                        break;
+                    case 1:
+                        selectFragment(FirstYearAction);
+                        break;
+                    case 2:
+                        selectFragment(SecondYearAction);
+                        break;
+                    case 3:
+                        selectFragment(ThirdYearAction);
+                        break;
+                    case 4:
+                        selectFragment(FourthYearAction);
+                        break;
                 }
             }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
+
+
+
+    }
+
+    private void selectFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.RoomsFrameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
